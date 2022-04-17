ddeaf611896a2f27865057a1119870ee0ee38825
Adetonwa Richard <96427062+Peculiar-codes@users.noreply.github.com>
2022-04-17 22:51:20 +0100

    Add files via upload

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,44 +1,50 @@
 
-// Files to cache
-const cacheName = 'quiz app';
-const appShellFiles = [
-    index.html,
-    styles/style.css,
-    scripts/script.js,
-    scripts/quiz.js,
-    peculiar.js,
-    icons,
-    images,
-    images/loader.jpg,
-    images/img5.jpg,
-    questions.json
-  /*css,js,images,icon..*/
-];
-/*const gamesImages = [];
-for (let i = 0; i < games.length; i++) {
-  gamesImages.push(`data/img/${games[i].slug}.jpg`);
-}*/
-const contentToCache = appShellFiles//.concat(gamesImages);
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'https://peculiar-codes.github.io/Quiz-App/index.html';
 
-// Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+   
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
   })());
 });
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
   })());
+
+  
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+       
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
 });

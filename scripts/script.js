9d2f59130ae537e4a7e01c461e4ba3089774c3d1
Adetonwa Richard <96427062+Peculiar-codes@users.noreply.github.com>
2022-04-18 09:43:43 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -91,6 +91,62 @@ p("#english").on("click",()=>{
     
         
     });
+    const scrollElements = document.querySelectorAll(".scroll");
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+  throttleTimer = true;
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
+}
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+window.addEventListener("scroll", () => { 
+  
+  throttle(() => {
+    handleScrollAnimation();
+   
+  }, 250);
+});
+
     
  }   
     
